8a5e679cbc8b120dbd9082df4bb841eb4723cf0c
Rainer M. Krug <Rainer@krugs.de>
2019-11-20 16:28:16 +0100

    changes in aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -3,13 +3,14 @@
 #' @export
 #'
 .onAttach <- function(libname, pkgname) {
+  browser()
   ver <- utils::packageDescription(
     "emeScheme",
     fields = c( "schemeName", "schemeVersion" )
   )
 
   if ( !scheme_installed( ver$schemeName, ver$schemeVersion) ) {
-    dmdScheme::scheme_install("emeScheme", "0.9.5", repo = "https://github.com/Exp-Micro-Ecol-Hub/dmdSchemeRepository/raw/master/")
+    dmdScheme::scheme_install(ver$schemeName, ver$schemeVersion, repo = "https://github.com/Exp-Micro-Ecol-Hub/dmdSchemeRepository/raw/master/")
   }
 
   scheme_use( name = ver$schemeName, version = ver$schemeVersion)
