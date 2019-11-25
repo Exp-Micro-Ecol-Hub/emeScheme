c6758af29f6f48215153a67b8205bb5702ed69a3
Rainer Krug <Rainer@krugs.de>
2019-11-25 13:47:41 +0100

    Fix documentation

--- R/as_xml_list.emeSchemeSet.R
+++ R/as_xml_list.emeSchemeSet.R
@@ -1,5 +1,13 @@
 #' Convert \code{emeSchemeSet} to an \code{xml_list} object
 #'
+#' @param x object to be converted.
+#' @param output specifies the content and format of the exported xml.
+#' \describe{
+#'   \item{"metadata" : }{export of the metadata only with no format attributes}
+#'   \item{"complete" : }{export tof the complete sheme, i.e. "metadata" plus the scheme definition. This is a self contained format which contains all attributes.}
+#' }
+#' @param ... additional arguments for methods
+#'
 #' @rdname as_xml_list
 #' @export
 #'
