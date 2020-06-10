731fdd6abbcaa6b4426fdec7e3684745f5f2a720
Rainer M. Krug <Rainer@krugs.de>
2020-06-10 10:17:09 +0200

    Moves SCHEME_PACKAGES into https://github.com/Exp-Micro-Ecol-Hub/emeScheme_schemePackage

--- SCHEME_PACKAGE/install_R_package.R
+++
@@ -1,8 +0,0 @@
-rold <- options("repos")
-r <- rold
-r["dmdScheme"] <- "https://exp-micro-ecol-hub.github.io/dmdSchemeRMaster/"
-options(repos = r)
-
-install.packages("emeScheme")
-
-options(repos = rold)
